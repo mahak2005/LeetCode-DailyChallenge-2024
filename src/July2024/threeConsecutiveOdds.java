221f5afeab5844f9357768a529909fd40a1640d9
Mahak <mahakyadav0520@gmail.com>
2024-07-02 23:51:41 +0530

    1st July Daily challenge

--- src/July2024/threeConsecutiveOdds.java
+++ src/July2024/threeConsecutiveOdds.java
@@ -2,24 +2,24 @@ package July2024;
 
 public class threeConsecutiveOdds {
     public boolean threeConsecutiveOdds(int[] arr) {
-        for(int i=0;i<arr.length-2;i++){
-            if(arr[i]%2==1 && arr[i+1]%2==1 && arr[i+2]%2==1){
-                return true;
-            }
-        }
-        return false;
+//        for(int i=0;i<arr.length-2;i++){
+//            if(arr[i]%2==1 && arr[i+1]%2==1 && arr[i+2]%2==1){
+//                return true;
+//            }
+//        }
+//        return false;
 
-        // for(int i=0;i<arr.length-2;i++){
-        //     int count=0;
-        //     for(int j=i;j<i+3;j++){
-        //         if(arr[j]%2!=0){
-        //             count++;
-        //         }
-        //     }
-        //     if(count==3){
-        //         return true;
-        //     }
-        // }
-        // return false;
+         for(int i=0;i<arr.length-2;i++){
+             int count=0;
+             for(int j=i;j<i+3;j++){
+                 if(arr[j]%2!=0){
+                     count++;
+                 }
+             }
+             if(count==3){
+                 return true;
+             }
+         }
+         return false;
     }
 }
