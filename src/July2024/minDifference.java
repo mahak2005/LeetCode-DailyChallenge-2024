8571354b1497b36d4ef0671401bfdf14b7f80675
Mahak <mahakyadav0520@gmail.com>
2024-07-03 22:32:28 +0530

    3rd July Daily challenge

---
+++ src/July2024/minDifference.java
@@ -0,0 +1,18 @@
+package July2024;
+
+import java.util.Arrays;
+
+public class minDifference {
+    public int minDifference(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        if(n<=4){
+            return 0;
+        }
+        int op1=nums[n-4]-nums[0];
+        int op2=nums[n-3]-nums[1];
+        int op3=nums[n-2]-nums[2];
+        int op4=nums[n-1]-nums[3];
+        return Math.min(Math.min(op1,op2),Math.min(op3,op4));
+    }
+}
